0964fdc8e7aac2225ce931feeb5ec1cb944ef06a
Easily JS <easilyjs@gmail.com>
2022-09-20 22:51:00 +0800

    minor: support preview

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,14 @@
+import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import alias from '@rollup/plugin-alias';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), alias()],
+  resolve: {
+    alias: {
+      'react-happy-form': path.resolve(__dirname, '..'),
+    },
+  },
 });
