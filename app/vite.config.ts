ab239a505bdbb091bc8ee2de18a6cb9cdd16fc5f
Easily JS <easilyjs@gmail.com>
2022-09-21 15:16:22 +0800

    minor: try build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,19 @@
-import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import alias from '@rollup/plugin-alias';
+// import alias from '@rollup/plugin-alias';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), alias()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      'react-happy-form': path.resolve(__dirname, '..'),
+      'react-happy-form': '../..',
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [resolve()],
     },
   },
 });
