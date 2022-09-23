2997e6d3dffeefd55eed71e838bd90f8c903098c
Ying <ying.lin@compass.com>
2022-09-23 11:42:17 +0800

    fix

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -96,7 +96,6 @@ function setPropertyValue(
  * Gets the value at `path` of `object`. If the resolved value is
  * `undefined`, the `defaultValue` is returned in its place.
  *
- * @category Object
  * @param {Object} object The object to query.
  * @param {string} path The path of the property to get.
  * @param {*} [defaultValue] The value returned for `undefined` resolved values.
