f6afc25a812ea2d0e879c1299b1b531f3e370180
Xusheng Yang <easilyjs@gmail.com>
2023-02-27 22:34:18 +0800

    minor: simplify ref

--- src/wrappers/native.ts
+++ src/wrappers/native.ts
@@ -1,56 +1,12 @@
 import { FormField } from '../types';
 
-type NativeType = 'text' | 'select' | 'radio' | 'checkbox';
-
-type NativeOptions = {
-  type?: NativeType;
-  value?: any;
-  valueKey?: any;
-  defaultValue?: any;
-};
-
-export const native = (field: FormField, options?: NativeOptions) => {
-  const { type, value, valueKey, defaultValue } = options || {};
-
-  switch (type) {
-    case 'radio':
-      return {
-        ...field,
-        value: undefined,
-        [valueKey ?? 'checked']: field.value === value,
-        onChange: () => {
-          field.onChange(value);
-          // trigger touched when radio change
-          field.onBlur?.();
-        },
-        ref: field.ref ? (ref: any) => field.ref(ref, value) : undefined,
-      };
-    case 'checkbox':
-      const fieldValue: any[] = field.value || [];
-      const checked = fieldValue.includes(value);
-      return {
-        ...field,
-        value: undefined,
-        [valueKey ?? 'checked']: checked,
-        onChange: () => {
-          field.onChange(
-            checked
-              ? fieldValue.filter((item) => item !== value)
-              : [...fieldValue, value],
-          );
-          // trigger touched when checkbox change
-          field.onBlur?.();
-        },
-        ref: field.ref ? (ref: any) => field.ref(ref, value) : undefined,
-      };
-    default:
-      return {
-        ...field,
-        value: field.value ?? defaultValue ?? '',
-        // use any to support custom native like event
-        onChange: (event: any) => {
-          field.onChange(event.target.value);
-        },
-      };
-  }
+export const native = (field: FormField) => {
+  return {
+    ...field,
+    value: field.value ?? '',
+    // use any to support native like event
+    onChange: (event: any) => {
+      field.onChange(event.target.value);
+    },
+  };
 };
